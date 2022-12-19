64326ba2539c5fc2cd50ab651a0de26ddb493900
kenzy yacini <91525930+Eberlouedd@users.noreply.github.com>
2022-12-19 20:27:17 +0100

    Update server.c

--- server.c
+++ server.c
@@ -1,62 +1,20 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyacini <kyacini@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 16:43:20 by kyacini           #+#    #+#             */
+/*   Updated: 2022/12/19 20:23:03 by kyacini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
 #include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	if (!str)
-		return (0);
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
-char	*ft_realloc(char *str, char c)
-{
-	char	*new;
-	int	i;
-
-	result = ft_calloc((ft_strlen(str) + 2), 1);
-	if (!result)
-		return (NULL);
-	i = 0;
-	while (str[i])
-	{
-		result[i] = str[i];
-		i++;
-	}
-	result[i] = c;
-	free(str);
-	return (result);
-}
-
-char *receive_message(int signal)
-{
-	char *message;
-	static int count = 0;
-
-	if (!message)
-		message = ft_calloc(1, 1);
-	if (signal == SIGUSR1)
-	{
-		ft_realloc(message, '1');
-		count = 0;
-	}
-	else
-	{
-		ft_realloc(message, '0');
-		count++;
-	}
-	if(count == 8)
-	{
-		printf("%s\n", message);
-	}
-	free(message);
-}
 
 int power2(int n)
 {
@@ -91,7 +49,6 @@ char	*btoc(char *str)
 	i = 7;
 	n = 0;
 	decripted = malloc(ft_strlen(str) / 8 + 1);
-	decripted[ft_strlen(str) / 8] = '\0';
 	while(str[i])
 	{
 		decripted[n] = btoi(str, i);
@@ -101,9 +58,43 @@ char	*btoc(char *str)
 	return (decripted);
 }
 
+void receive_message(int signal)
+{
+	static char *message;
+	static int count = 0;
+	static char *final = NULL;
+
+	if (!message)
+		message = ft_calloc(1,1);
+	if (signal == SIGUSR1)
+	{
+		message = ft_realloc(message, '1');
+		count++;
+	}
+	else if (signal == SIGUSR2)
+	{
+		message = ft_realloc(message, '0');
+		count++;
+	}
+	if(count == 8)
+	{
+		final = ft_strjoin(final, btoc(message));
+		printf("%s", final);
+		if(ft_strcmp(message, "00000000") == 0)
+		{
+			printf("%s", final);
+			free(final);
+			final = NULL;	
+		}
+		free(message);
+		message = NULL;
+		count = 0;
+	}
+}
+
 int main()
 {
-	ft_printf("PID : %d\n", getpid());
+	printf("PID : %d\n", getpid());
 	while (1)
 	{
 		signal(SIGUSR1, receive_message);
@@ -111,3 +102,4 @@ int main()
 		pause();
 	}
 }
+
