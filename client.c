0ae71e0232086187a7294325d554a06d15413bdc
kenzy yacini <91525930+Eberlouedd@users.noreply.github.com>
2022-12-19 20:26:36 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,54 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyacini <kyacini@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 16:43:09 by kyacini           #+#    #+#             */
+/*   Updated: 2022/12/19 20:23:41 by kyacini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
 #include <stdio.h>
-#include <stdlib.h>
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	if (!str)
-		return (0);
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*new_chain;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	if (!s2)
-		return (NULL);
-	new_chain = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!new_chain)
-		return (NULL);
-	new_chain[ft_strlen(s1) + ft_strlen(s2)] = '\0';
-	while (j < ft_strlen(s2) || (s2[0] == '\0' && j <= ft_strlen(s2)))
-	{
-		while (i < ft_strlen(s1))
-		{
-			new_chain[i] = s1[i];
-			i++;
-		}
-		new_chain[i] = s2[j];
-		i++;
-		j++;
-	}
-	return (new_chain);
-}
-
+#include <sys/types.h>
+#include <signal.h>
 
 void send_message(int pid, char *str)
 {
 	int	i;
 
 	i = 0;
+	printf("%s\n", str);
 	while(str[i])
 	{
 		if(str[i] == '1')
@@ -56,6 +28,7 @@ void send_message(int pid, char *str)
 		else
 			kill(pid, SIGUSR2);
 		i++;
+		usleep(150);
 	}
 }
 
@@ -98,7 +71,7 @@ int main(int argc, char **argv)
 	char *message;
 
 	message = conv_bin(argv[2]);
-	send_message(argv[1], message);
+	send_message(ft_atoi(argv[1]), message);
 	free(message);
 	return (0);
 }
